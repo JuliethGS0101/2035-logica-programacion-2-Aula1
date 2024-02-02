d4fd490f67f41f6f7a60497ce9b2620aed1295bb
JuliethGS0101 <156958433+JuliethGS0101@users.noreply.github.com>
2024-02-02 17:47:31 -0500

    aula1 ejercicios

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
  let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del numero secreto';
+titulo.innerHTML = 'Hora del Desafío';
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un numero del 1 al 10';
@@ -7,4 +7,19 @@ parrafo.innerHTML = 'Indica un numero del 1 al 10';
 function intentoUsuario() {
     alert('Click desde el boton');
 }
-
+function mostarMensajeEnLaConsola() {
+    console.log('El botón fue clicado!')
+}
+function mostrarAlerta() {
+    let ciudad = prompt("Por favor, ingresa el nombre de una ciudad de Brasil:");
+    alert("Estuve en " + ciudad + " y me acordé de ti.");
+}
+function mostarAlert(){
+    alert("AMO JS");
+} 
+function sumaNumeros(){
+    let primerNumero = parseInt(prompt('Digite el primer numero'));
+    let segunNumero = parseInt(prompt('Digite el segundo numero'));
+    let resultado = primerNumero + segunNumero;
+    alert(`${primerNumero} + ${segunNumero} = ${resultado}`)
+}
